c1238bf15375298fff92db077bcb5fd91182df8a
felipe letelier <pipe27febrero@hotmail.com>
2019-04-28 15:55:56 -0400

    clase 11 finalizada

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
 
 const container = document.getElementById('app');
-ReactDOM.render(<Badge/>, container);
+ReactDOM.render(<Badge firstName="felipe" lastName="letelier" jobTitle="Frontend Engineer" twitter="felipeletelier" avatarUrl="https://www.gravatar.com/avatar?d=identicon"/>, container);
