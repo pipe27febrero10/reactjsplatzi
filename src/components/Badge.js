11cad40019f286ab6ecf814fb023dceaea4de1d5
felipe letelier <pipe27febrero@hotmail.com>
2019-04-27 23:03:48 -0400

    mejorar la badge con estilos e instalar bootstrap

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,22 +2,23 @@
 
 import React from 'react';
 import confLogo from '../images/badge-header.svg';
+import  './styles/Badge.css';
 
 class Badge extends React.Component{
   render(){
-  return <div>
-            <div>
+  return <div className="Badge"> 
+            <div className="Badge__header">
               <img src={confLogo} alt="Logo de la conferencia"/> 
             </div>
-            <div>
-              <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+            <div className="Badge__section-name">
+              <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
               <h1> Felipe <br/> Letelier </h1>
             </div>
-            <div>
+            <div className="Badge__section-info">
               <p> Front-End Engineer</p>
               <p> @FelipeLetelier</p>
             </div>
-            <div>
+            <div className="Badge__footer">
               #platziconf
             </div>
          </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Badge from './components/Badge'
+import Badge from './components/Badge';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const container = document.getElementById('app');
 ReactDOM.render(<Badge/>, container);
